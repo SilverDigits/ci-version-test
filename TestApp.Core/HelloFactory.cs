8d5f058ee923a9dcc6bbe161089eb7b9157303a0
Jason Kitchen <jason@silverdigits.com>
2026-01-20 21:48:30 -0500

    ci: test

---
+++ TestApp.Core/HelloFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp.Core
+{
+    public static class HelloFactory
+    {
+
+        public static string CreateHelloMessage(string name)
+        {
+            return $"Hello, {name}!";
+        }
+
+    }
+}
---
+++ TestAppTwo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestAppTwo.WebApiApplication" Language="C#" %>
